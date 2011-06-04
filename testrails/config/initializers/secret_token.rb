cbbb5b9f26c9038d66d92b3dc6ef313d6a5c4fc5
Kazuya Kishi <kishikazuya@gmail.com>
2011-06-04 16:05:27 +0900

    add rails project

---
+++ testrails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kishikazuya::Application.config.secret_token = '5a9cc8ee58bca337b4b8e705981ae82eee6cea75d170ec3e6c297778d5f6c46a1dc3d2fe474d6e38d345ad3d4a9bfd4012eff210973f956bde818326ffd8aae5'
